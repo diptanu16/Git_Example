1869a4032a0d22003ea180048b00d699a094ad9d
Diptanu <diptanudnath@gmail.com>
2021-10-30 12:24:02 +0530

    Added Multiply.java file

---
+++ Multiply.java
@@ -0,0 +1,7 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println("The multiplication of two numbers is" +(a*b));
+    }
+}
